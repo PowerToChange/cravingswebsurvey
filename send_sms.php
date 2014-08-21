0a9ecb87114f56dc7ea55ccd2b904a0251443674
jacques <jacques.robitaille@gmail.com>
2014-08-21 15:19:51 -0400

    added send_sms.php

---
+++ send_sms.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    
+  </head>
+  <body>
+<?php
+require('lib/twilio-php/Services/Twilio.php'); 
+require('civi_constants.php'); 
+
+$client = new Services_Twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN); 
+
+$firstname = 'T';
+if(isset($_POST['submitted']['civicrm_1_contact_1_fieldset_fieldset']['civicrm_1_contact_1_contact_first_name']))
+{
+  $firstname = $_POST['submitted']['civicrm_1_contact_1_fieldset_fieldset']['civicrm_1_contact_1_contact_first_name'] . ', t';  
+}
+
+if(isset($_POST['submitted']['civicrm_1_contact_1_fieldset_fieldset']['civicrm_1_contact_1_phone_phone']))
+{
+  $phone = '+1' . $_POST['submitted']['civicrm_1_contact_1_fieldset_fieldset']['civicrm_1_contact_1_phone_phone'];
+    
+  $client->account->messages->create(array( 
+    'To' => $phone, 
+    'From' => "+12267800039", 
+    'Body' => "{$firstname}, thanks for completing a Power to Change survey!  Explore our mycravings website at: http://mycravings.ca",   
+  ));
+
+}
+
+?>
+  </body>
+</html>
